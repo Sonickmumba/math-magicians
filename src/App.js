a872c593bbd2bdc829cd9633a59bfbe3d16c0205
sonickmumba <kingellie.mumba@gmail.com>
2022-09-22 11:18:16 +0200

    Refactor calculator component with hooks

--- src/App.js
+++ src/App.js
@@ -2,24 +2,8 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      calculationData: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
-  }
-
-  render() {
-    const { calculationData } = this.state;
-    return (
-      <Calculator calculationData={calculationData} />
-    );
-  }
-}
+const App = () => (
+  <Calculator />
+);
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,83 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      next: null,
-      total: '0',
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const [object, setObject] = useState({
+    next: null,
+    total: '0',
+    operation: null,
+  });
 
-  handleClick = (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
-    const { next, total, operation } = calculate(this.state, e.target.name);
+    const { next, total, operation } = calculate(object, e.target.name);
 
     if (next === null && total === null) {
-      this.setState({ next, total: '0', operation });
+      setObject({ next, total: '0', operation });
     } else {
-      this.setState({ next, total, operation });
+      setObject({ next, total, operation });
     }
   };
 
-  render() {
-    const { total, next } = this.state;
-    const btnNames = [
-      'AC',
-      '+/-',
-      '%',
-      '÷',
-      '7',
-      '8',
-      '9',
-      'x',
-      '4',
-      '5',
-      '6',
-      '-',
-      '1',
-      '2',
-      '3',
-      '+',
-      '0',
-      '.',
-      '=',
-    ];
-    return (
-      <div className="calculator-grid-container">
-        { next ? (
-          <div className="output-result">
-            { next }
-          </div>
-        ) : (
-          <div className="output-result">
-            { total }
-          </div>
-        )}
-        { btnNames.map((btnName) => (
-          <button
-            onClick={(e) => this.handleClick(e)}
-            name={btnName}
-            type="button"
-            className={`btn ${btnName === '0' ? 'zero-button' : ''}
-            ${btnName === '=' ? 'button-operation' : ''}
-            ${btnName === '÷' ? 'button-operation' : ''}
-            ${btnName === 'x' ? 'button-operation' : ''}
-            ${btnName === '-' ? 'button-operation' : ''}
-            ${btnName === '+' ? 'button-operation' : ''}`}
-            key={btnName}
-          >
-            { btnName }
-          </button>
-        ))}
+  const { total, next } = object;
+  const btnNames = [
+    'AC',
+    '+/-',
+    '%',
+    '÷',
+    '7',
+    '8',
+    '9',
+    'x',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '0',
+    '.',
+    '=',
+  ];
 
-      </div>
-    );
-  }
-}
+  return (
+    <div className="calculator-grid-container">
+      { next ? (
+        <div className="output-result">
+          { next }
+        </div>
+      ) : (
+        <div className="output-result">
+          { total }
+        </div>
+      )}
+      { btnNames.map((btnName) => (
+        <button
+          onClick={handleClick}
+          name={btnName}
+          type="button"
+          className={`btn ${btnName === '0' ? 'zero-button' : ''}
+          ${btnName === '=' ? 'button-operation' : ''}
+          ${btnName === '÷' ? 'button-operation' : ''}
+          ${btnName === 'x' ? 'button-operation' : ''}
+          ${btnName === '-' ? 'button-operation' : ''}
+          ${btnName === '+' ? 'button-operation' : ''}`}
+          key={btnName}
+        >
+          { btnName }
+        </button>
+      ))}
+    </div>
+  );
+};
 
 export default Calculator;
