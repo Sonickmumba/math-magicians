414bd445a57de210d4fc59301509e53b59e70a05
sonickmumba <kingellie.mumba@gmail.com>
2022-09-28 10:38:30 +0200

    Add Header.js, Home.js, NavLinks.js and Quote.js components in the pages folder

---
+++ src/pages/Quote.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Quote = () => (
+  <p className="math-quote">
+    Mathematics is about numbers, equations, computations, or algorithms:
+    Its about understanding. - Williams Paul Thurston
+  </p>
+);
+
+export default Quote;
