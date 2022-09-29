ee3b104f4dbb1cdf9d0723ee0f63c736a365ad17
sonickmumba <kingellie.mumba@gmail.com>
2022-09-29 11:43:44 +0200

    Fix linter error

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Quote = () => (
   <p className="math-quote">
-    Mathematics is about numbers, equations, computations, or algorithms:
+    Mathematics is not about numbers, equations, computations, or algorithms:
     Its about understanding. - Williams Paul Thurston
   </p>
 );
