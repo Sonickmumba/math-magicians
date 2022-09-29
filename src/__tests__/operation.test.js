238bf7a87d6c5d0c916f8ae2237218b9edaf8d6e
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-09-29 15:52:09 +0300

    Fix the linter error

--- src/__tests__/operation.test.js
+++ src/__tests__/operation.test.js
@@ -1,14 +1,14 @@
 import operate from '../logic/operate';
 
 test('Addition', () => {
-    expect(operate(4, 5, '+')).toBe('9');
+  expect(operate(4, 5, '+')).toBe('9');
 });
 test('Subtraction', () => {
-    expect(operate(18, 8, '-')).toBe('10');
+  expect(operate(18, 8, '-')).toBe('10');
 });
 test('Multipication', () => {
-    expect(operate(10, 8, 'x')).toBe('80');
+  expect(operate(10, 8, 'x')).toBe('80');
 });
 test('Division', () => {
-    expect(operate(10, 5, '÷')).toBe('2');
+  expect(operate(10, 5, '÷')).toBe('2');
 });
