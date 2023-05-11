5c848864308abdcd2db8f673798d7935629bd4c5
sonickmumba <kingellie.mumba@gmail.com>
2023-05-11 13:47:56 +0200

    Updates

--- src/pages/NavLinks.js
+++ src/pages/NavLinks.js
@@ -5,11 +5,11 @@ const NavLinks = () => (
   <nav id="navbar-container">
     <ul className="navbar-items">
       <li className="nav-link">
-        <Link to="/home">Home</Link>
+        <Link to="/">Calculator</Link>
       </li>
       <span className="vertical-line" />
       <li className="nav-link">
-        <Link to="/">Calculator</Link>
+        <Link to="/home">Home</Link>
       </li>
       <span className="vertical-line" />
       <li className="nav-link">
