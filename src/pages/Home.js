cad599b130707e4aaf2416bb81baa52cef9e7f04
Mumba <27748812@sun.ac.za>
2023-05-11 16:37:28 +0200

    More update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,13 @@ const Home = () => (
   <main id="home-link-container">
     <h2 className="home-title">Welcome To Our Page</h2>
     <p className="home-link-text">
-      Welcome to the Math Magician application! Get ready to unlock the secrets 
-      of numbers, unravel complex equations, and explore the fascinating world of 
-      mathematics. Whether you're looking for quick calculations, mathematical insights, 
-      or challenging problems, I'm here to assist you on your magical math journey. Let's 
-      embark on this adventure together and make math fun and enchanting!
+      Welcome to the Math Magician application! Get ready to unlock the secrets of
+      numbers, unravel complex equations, and explore the fascinating world of mathematics.
+      Whether you're looking for quick calculations, mathematical insights,
+      or challenging problems, I'm here to assist you on your magical math journey.
+      Let's embark on this adventure together and make math fun and enchanting!
     </p>
     <p className="home-link-text">
-      
     </p>
   </main>
 );
